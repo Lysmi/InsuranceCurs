bb501647e1086078aafea628a8dddfa8b0768c19
Lysmi <lysmi.mx@mail.ru>
2021-11-21 03:25:46 +0300

    Init

---
+++ ClientAdd.Designer.cs
@@ -0,0 +1,251 @@
+﻿
+namespace InsuranceCurs
+{
+    partial class ClientAdd
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.cityBox = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.firstNameInput = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.surnameInput = new System.Windows.Forms.TextBox();
+            this.patronimicInput = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.socialBox = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.BirhdayPicker = new System.Windows.Forms.DateTimePicker();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.houseInput = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.phoneInput = new System.Windows.Forms.TextBox();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.errorDate = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.errorDate)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cityBox
+            // 
+            this.cityBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cityBox.FormattingEnabled = true;
+            this.cityBox.Location = new System.Drawing.Point(73, 20);
+            this.cityBox.Name = "cityBox";
+            this.cityBox.Size = new System.Drawing.Size(121, 21);
+            this.cityBox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "City";
+            // 
+            // firstNameInput
+            // 
+            this.firstNameInput.Location = new System.Drawing.Point(73, 60);
+            this.firstNameInput.Name = "firstNameInput";
+            this.firstNameInput.Size = new System.Drawing.Size(110, 20);
+            this.firstNameInput.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "FirstName";
+            // 
+            // surnameInput
+            // 
+            this.surnameInput.Location = new System.Drawing.Point(73, 100);
+            this.surnameInput.Name = "surnameInput";
+            this.surnameInput.Size = new System.Drawing.Size(110, 20);
+            this.surnameInput.TabIndex = 4;
+            // 
+            // patronimicInput
+            // 
+            this.patronimicInput.Location = new System.Drawing.Point(73, 138);
+            this.patronimicInput.Name = "patronimicInput";
+            this.patronimicInput.Size = new System.Drawing.Size(110, 20);
+            this.patronimicInput.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 100);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Surname";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 141);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Patronimic";
+            // 
+            // socialBox
+            // 
+            this.socialBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.socialBox.FormattingEnabled = true;
+            this.socialBox.Location = new System.Drawing.Point(73, 180);
+            this.socialBox.Name = "socialBox";
+            this.socialBox.Size = new System.Drawing.Size(121, 21);
+            this.socialBox.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 180);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(36, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Social";
+            // 
+            // BirhdayPicker
+            // 
+            this.BirhdayPicker.Location = new System.Drawing.Point(73, 212);
+            this.BirhdayPicker.Name = "BirhdayPicker";
+            this.BirhdayPicker.Size = new System.Drawing.Size(200, 20);
+            this.BirhdayPicker.TabIndex = 10;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 218);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(45, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Birthday";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 250);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(38, 13);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "House";
+            // 
+            // houseInput
+            // 
+            this.houseInput.Location = new System.Drawing.Point(73, 247);
+            this.houseInput.Name = "houseInput";
+            this.houseInput.Size = new System.Drawing.Size(199, 20);
+            this.houseInput.TabIndex = 13;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(12, 281);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(38, 13);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "Phone";
+            // 
+            // phoneInput
+            // 
+            this.phoneInput.Location = new System.Drawing.Point(73, 278);
+            this.phoneInput.Name = "phoneInput";
+            this.phoneInput.Size = new System.Drawing.Size(199, 20);
+            this.phoneInput.TabIndex = 15;
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(119, 316);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(75, 23);
+            this.saveButton.TabIndex = 16;
+            this.saveButton.Text = "Сохранить";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // errorDate
+            // 
+            this.errorDate.ContainerControl = this;
+            // 
+            // ClientAdd
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(308, 351);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.phoneInput);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.houseInput);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.BirhdayPicker);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.socialBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.patronimicInput);
+            this.Controls.Add(this.surnameInput);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.firstNameInput);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cityBox);
+            this.Name = "ClientAdd";
+            this.Text = "AddElem";
+            ((System.ComponentModel.ISupportInitialize)(this.errorDate)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cityBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox firstNameInput;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox surnameInput;
+        private System.Windows.Forms.TextBox patronimicInput;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox socialBox;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DateTimePicker BirhdayPicker;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox houseInput;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox phoneInput;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.ErrorProvider errorDate;
+    }
+}
